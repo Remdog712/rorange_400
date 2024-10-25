b65e8b8a5079ccf213499d167b9f1d7f469f7065
Remington Orange <remingtonorange@yahoo.com>
2024-10-25 00:24:21 -0400

    JavaExercise2 and SVG Assignment

--- docs/script.js
+++ docs/script.js
@@ -6,11 +6,53 @@ function init() {
     var fontColorButton = document.getElementById('fontColorButton');
     var boldButton = document.getElementById('hightlightbutton');
     var bgColorButton = document.getElementById('bgColorButton');
-    
+    var toggleColorCheckbox = document.getElementById('toggleColorCheckbox');
+    var removeTextCheckbox = document.getElementById('removeTextCheckbox');
+   
     fontColorButton.addEventListener('click', changeFontColor, false);
     boldButton.addEventListener('click', highlightParagraph, false);  
     bgColorButton.addEventListener('click', changeBackgroundColor, false);
+
+    toggleColorCheckbox.addEventListener('change', toggleName, false);
+    removeTextCheckbox.addEventListener('change', toggleParagraphTextColor, false);
+
+
+
+// Javascript Exercise 2 - Start
+    highlightNamesCheckbox.addEventListener('change', toggleHighlight, false);
+    highlightPlacesCheckbox.addEventListener('change', toggleHighlight, false);
+    highlightHobbyCheckbox.addEventListener('change', toggleHighlight, false);
 }
+
+function toggleName() {
+    var mainTitle = document.getElementById('mainTitle');
+    mainTitle.style.color = this.checked ? "orange" : ""; 
+}
+
+function toggleParagraphTextColor() {
+    var paragraph = document.getElementById('toggleTextParagraph');
+    paragraph.style.color = this.checked ? "lightgrey" : ""; // Hehe it's just a prank
+}
+
+function toggleHighlight() {
+    var targetClass;
+    switch (this.id) {
+        case "highlightNamesCheckbox":
+            targetClass = "name";
+            break;
+        case "highlightPlacesCheckbox":
+            targetClass = "place";
+            break;
+        case "highlightHobbyCheckbox": 
+            targetClass = "hobby";
+            break;
+    }
+    var elements = document.getElementsByClassName(targetClass);
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.toggle("highlight");
+    }
+}
+// Javascript Exercise 2 - End
 
 function changeFontColor() {
     var sectionTitle = document.querySelector('#testing-js h2');
@@ -24,5 +66,5 @@ function highlightParagraph() {
 
 function changeBackgroundColor() {
     var section = document.getElementById('testing-js');
-    section.style.backgroundColor = "orange";
+    section.style.backgroundColor = "#A2AAAD";
 }
